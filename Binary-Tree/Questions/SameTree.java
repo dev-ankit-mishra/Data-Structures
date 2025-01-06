7da83185d06b26423057a61c7fd462516401648e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-01-06 23:12:55 +0530

    Same Tree[LeetCode 100]

---
+++ Binary-Tree/Questions/SameTree.java
@@ -0,0 +1,26 @@
+
+//Problem Link- https://leetcode.com/problems/same-tree/description/
+package Questions;
+
+import Implementation.Node;
+
+public class SameTree {
+    public boolean isSameTree(Node p, Node q) {
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        else if(isSameTree(p.left,q.left)==false){
+            return false;
+        }
+        else if(isSameTree(p.right,q.right)==false){
+            return false;
+        }
+        return true;
+    }
+}
