122677c80d2a98f51db736761562da4383249ba7
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-01-08 19:32:10 +0530

    Level Order Traversal

---
+++ Binary-Tree/Implementation/LevelOrderTraversal.java
@@ -0,0 +1,18 @@
+package Implementation;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public void LevelOrder(TreeNode root){
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root!=null) q.add(root);
+        while (!q.isEmpty()){
+            TreeNode front=q.remove();
+            System.out.print(front.val + " ");
+            if(front.left!=null)q.add(front.left);
+            if(front.right!=null)q.add(front.right);
+        }
+    }
+
+}
