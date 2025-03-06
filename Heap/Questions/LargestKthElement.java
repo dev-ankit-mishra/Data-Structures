f99232eaf7271e474e327b09fb7106e62806b3d1
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-07 00:44:46 +0530

    Largest Kth Element[LeetCode-215]

---
+++ Heap/Questions/LargestKthElement.java
@@ -0,0 +1,18 @@
+//Problem Link-https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+import java.util.PriorityQueue;
+
+public class LargestKthElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        for(int ele: nums){
+            pq.add(ele);
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        return pq.peek();
+
+    }
+}
